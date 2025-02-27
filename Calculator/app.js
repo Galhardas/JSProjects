d0fbc5a6a9dcd9222db3c2809c78875e8536c5b8
Galhardas <davidmiguel_97@hotmail.com>
2025-02-27 23:35:34 +0000

    added "data tags" to html elements instead of classes. To dinguish what's been using by JS script and CSS styles. Started creating variables in JS script for button/element recongnition

---
+++ Calculator/app.js
@@ -0,0 +1,8 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operandButton = document.querySelectorAll('[data-operand]')
+const allClearButton = document.querySelectorAll('[data-all-clear]')
+const deleteButton = document.querySelectorAll('[data-delete]')
+const equalButton = document.querySelectorAll('[data-equal]')
+
+const previousOperandText = document.querySelectorAll('[data-previous-operand]')
+const currentOperandText = document.querySelectorAll('[data-current-operand]')
