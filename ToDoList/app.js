15a81190b6a770f524df7af340ebe49efd338478
Galhardas <davidmiguel_97@hotmail.com>
2025-02-27 16:50:14 +0000

    Added a "Done" button for every item. Added some margin on top and bottom of each new "li" item

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -34,6 +34,7 @@ function createItem(itemText, itemIndex){
 
     listItem.className = "todo";
     listItem.innerHTML = `
+    <button class="button-done">Done</button>
     <label for="${listItemID}">${itemText}</label>  <!-- for is to make the id "${listItemID}" react -->
     <button class="button-delete"><svg xmlns="http://www.w3.org/2000/svg" height="24px"
     // viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button>`;
