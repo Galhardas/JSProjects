39c4a8e6b080b87cf7d1bd73375db8ed345f9c1a
Galhardas <davidmiguel_97@hotmail.com>
2025-02-27 17:25:30 +0000

    Now the item (task) maintains it's class ".done" even after adding another item to the list

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -13,7 +13,7 @@ function addItem(){
     const itemText = Input.value.trim();
 
     if(itemText.length > 0){
-        allListItems.push(itemText);
+        allListItems.push({ text: itemText, done: false });;
         updateList();
         Input.value = '';
     }
@@ -24,19 +24,24 @@ function updateList(){
     ToDoList.innerHTML = "";
 
     allListItems.forEach((item, itemIndex)=>{
-        listItem = createItem(item, itemIndex);
+        listItem = createItem(item, itemIndex, item.done);
         ToDoList.append(listItem);
     })
 }
 
-function createItem(itemText, itemIndex){
+function createItem(item, itemIndex, isDone){
     const listItem = document.createElement("li");
     const listItemID = "todo-"+itemIndex;
 
-    listItem.className = "todo";
+    if (isDone) {
+        listItem.classList.add("done");
+    } else {
+        listItem.classList.add("todo");
+    }
+    
     listItem.innerHTML = `
     <button class="button-done">Done</button>
-    <label for="${listItemID}">${itemText}</label>  <!-- for is to make the id "${listItemID}" react -->
+    <label for="${listItemID}">${item.text}</label>  <!-- for is to make the id "${listItemID}" react -->
     <button class="button-delete"><svg xmlns="http://www.w3.org/2000/svg" height="24px"
     // viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button>`;
     
@@ -48,7 +53,8 @@ function createItem(itemText, itemIndex){
 
         const doneButton = listItem.querySelector(".button-done");
         doneButton.addEventListener('click', () => {
-            listItem.classList.toggle("done");
+            item.done = true;
+            updateList();
         })
     
     return listItem;
