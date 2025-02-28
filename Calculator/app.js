06a4c15af86f00cf5c7b1134eed92b4312e8ea08
Galhardas <davidmiguel_97@hotmail.com>
2025-02-28 00:22:48 +0000

    Still Buggy but it's getting better

--- Calculator/app.js
+++ Calculator/app.js
@@ -24,6 +24,7 @@ class Calculator {
     chooseOperation (operator) {
         this.previousOperandText.innerText = this.currentOperand.toString() + ' ' + operator.toString()
         this.currentOperandText.innerText = ''
+        this.currentOperand = ''
     }
 }
 
