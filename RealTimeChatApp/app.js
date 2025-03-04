e727678820c28a48f7f681857ee94f562decf4c5
Galhardas <davidmiguel_97@hotmail.com>
2025-03-04 23:23:37 +0000

    In RealTimeChatApp. With DeepSeek I got the websocket and server to be connected with the HTML. (Didn't understand this 100% yet)

--- RealTimeChatApp/app.js
+++ RealTimeChatApp/app.js
@@ -1,5 +1,13 @@
-const socket = io('http://localhost:2025')
+const socket = io(); // Connects automatically to the same origin
 
-socket.on('chat-message', data => {
-    console.log(data)
-})
+socket.on("chat-message", (msg) => {
+    console.log("Message from server:", msg);
+    document.getElementById("chat-box").innerHTML += `<p>${msg}</p>`;
+});
+
+document.getElementById("send-box").addEventListener("submit", (event) => {
+    event.preventDefault();
+    const message = document.getElementById("message-input").value;
+    socket.emit("chat-message", message);
+    document.getElementById("message-input").value = "";
+});
--- RealTimeChatApp/server.js
+++ RealTimeChatApp/server.js
@@ -1,5 +1,20 @@
-const io = require("socket.io")(2025)
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
-io.on("connect", socket => {
-    socket.emit('chat-message', 'Hello World')
-})
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve static files (index.html, style.css, app.js)
+app.use(express.static(__dirname));
+
+io.on("connection", (socket) => {
+    console.log("A user connected");
+    socket.emit("chat-message", "Hello World");
+});
+
+// Start server
+server.listen(2025, () => {
+    console.log("Server running on http://localhost:2025");
+});
