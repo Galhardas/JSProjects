4556090da1690a2ffe4688930148dd07a133cb1f
Galhardas <davidmiguel_97@hotmail.com>
2025-03-09 01:08:39 +0000

    Update

--- RealTimeChatApp/app.js
+++ RealTimeChatApp/app.js
@@ -1,5 +1,13 @@
 const socket = io('http://localhost:2025')
+const messageForm = document.getElementById('message-form')
+const messageInput = document.getElementById('message-input')
+const messageSend = document.getElementById('message-send')
 
 socket.on('chat-message', message => {
     console.log(message)
+})
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    messageInput.nodeValue = ''
 })
