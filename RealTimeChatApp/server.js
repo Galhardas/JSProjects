d63b9ba01b4edcf649a86f2bbb8c44ac7605e720
Galhardas <davidmiguel_97@hotmail.com>
2025-03-09 01:00:04 +0000

    Not sure if I'm back to when I started editing this today but let's roll

--- RealTimeChatApp/server.js
+++ RealTimeChatApp/server.js
@@ -1,6 +1,79 @@
-const io = require('socket.io')(2025)
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const { Server } = require('socket.io');
 
-io.on('connection', socket => {
-    console.log('Connected to server');
-    socket.emit('chat-message', 'Hello World')
-})
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+    // Serve index.html
+    if (req.url === '/') {
+        const filePath = path.join(__dirname, 'index.html');
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    }
+    // Serve app.js
+    else if (req.url === '/app.js') {
+        const filePath = path.join(__dirname, 'app.js');
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'application/javascript' });
+                res.end(data);
+            }
+        });
+    }
+    // Serve style.css
+    else if (req.url === '/style.css') {
+        const filePath = path.join(__dirname, 'style.css');
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/css' });
+                res.end(data);
+            }
+        });
+    }
+    // Serve socket.io.js from the client-dist folder
+    else if (req.url === '/socket.io/socket.io.js') {
+        const filePath = path.join(__dirname, 'node_modules', 'socket.io', 'client-dist', 'socket.io.js');
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'application/javascript' });
+                res.end(data);
+            }
+        });
+    }
+    // Handle other requests (404 Not Found)
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+    }
+});
+
+// Initialize Socket.IO
+const io = new Server(server);
+
+// Socket.IO connection handler
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.emit('chat-message', 'Hello World');
+});
+
+// Start the server on port 2025
+server.listen(2025, () => {
+    console.log('Server is running on http://localhost:2025');
+});
